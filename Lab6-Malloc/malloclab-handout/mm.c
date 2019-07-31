9a97de1b76534ca672057b55bc9d3ed6fbb5c021
chj <506933131@qq.com>
2019-07-31 14:56:57 +0800

    implement segregated list
    amptjp: 60 + 40 = 100
    binary2: 31 + 38 = 69
    binary : 33 + 40 = 73
    cccp: 60 + 40 = 100
    coalescing: 40 + 40 = 80
    cp-decl: 59 + 40 = 99
    expr: 60 + 40 = 100
    random2: 55 + 40 = 95
    random : 56 + 40 = 96
    realloc2: 27 + 40 = 67
    realloc : 29 + 40 = 69
    short1: 40 + 40 = 80
    short2: 54 + 40 = 94
    (better thru in segregated list, compared to explicit list)

--- Lab6-Malloc/malloclab-handout/mm.c
+++ Lab6-Malloc/malloclab-handout/mm.c
@@ -1,16 +1,21 @@
 /*
- * mm.c - implement explicit list
+ * mm.c - implement segregated list
  * 
- * In this approach, we still have header and footer for used or free block.
+ *     In this approach, we still have header and footer for used or free block.
  * (header for size info, footer for coalescing)
  * 
- * In a comparsion to implicit list, explicit list maintains a list only 
+ *     In a comparsion to implicit list, explicit list maintains a list only 
  * contain free blocks. So mm_malloc could be faster by just seraching on
  * free lists, while it still takes linear time complexity.
- * 
- * The key point is we place 2 pointers, which point to the prev and next free 
+ *     The key point is we place 2 pointers, which point to the prev and next free 
  * block, in a free block so that it knows where the prev and next free block
  * are.
+ * 
+ *     In a comparsion to explicit list, segregated list maintains multiple lists
+ * instead of a single list to search for free blocks. So mm_malloc could be 
+ * faster by firstly comparing req size to some fixed block size.
+ *     The key point is we maintain multiple free lists. Other designs remain the
+ * same with explicit list.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -78,18 +83,22 @@ team_t team = {
 static char *heap_listp = NULL;
 
 /* static pointer that points to start of free blocks */
-static char *heap_freep;
+#define FCNT 9
+#define FS(x) (4 * DSIZE * (1 << (x)))
+static char *heap_freep[FCNT];
 
 /* static functions */
 static void *extend_heap(size_t words);
 
 /* explicit free list manipulation */
 static inline void ex_delete(void *bp);
-static void ex_insert(void *bp);
-static void ex_insert_after(void *prev_bp, void *bp);
-static void *ex_coalesce(void *bp);
+static void *ex_insert(void *bp);
+static void ex_insert_after(void *prev_bp, void *bp, int class);
+static inline void *ex_insert_back(void *bp, int class);
 static inline void *ex_find_fit(size_t asize);
 static void ex_place(void *bp, size_t asize);
+static inline int ex_classify(size_t size);
+static inline int ex_update(void *bp, size_t newsize);
 
 /* 
  * mm_init - initialize the malloc package.
@@ -106,7 +115,9 @@ int mm_init(void)
     heap_listp += 2 * WSIZE;
 
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
-    heap_freep = NULL;
+    int i;
+    for (i = 0; i < FCNT; i++)
+        heap_freep[i] = NULL;
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
 
@@ -133,15 +144,7 @@ static void *extend_heap(size_t words)
 
     /* add the new free block from the end */
     /* coalesce if the previous block was free */
-    if (heap_freep)
-    {
-        ex_insert_after(GET_PREV(heap_freep), bp);
-        bp = ex_coalesce(bp);
-    }
-    else
-        ex_insert_after(NULL, bp);
-
-    return bp;
+    return ex_insert_back(bp, ex_classify(size));
 }
 
 /* 
@@ -195,7 +198,6 @@ void mm_free(void *bp)
     PUT(FTRP(bp), PACK(size, 0));
 
     ex_insert(bp);
-    ex_coalesce(bp);
 }
 
 /*
@@ -220,7 +222,7 @@ void *mm_realloc(void *ptr, size_t size)
     /* no need to call malloc if newsize <= oldsize */
     if (newsize <= oldsize)
     {
-        /* split the block */
+        /* split the block (3*DSIZE may be bigger) */
         if (oldsize - newsize >= 3 * DSIZE)
         {
             PUT(HDRP(bp), PACK(newsize, 1));
@@ -246,68 +248,39 @@ void *mm_realloc(void *ptr, size_t size)
     size_t free_size = prev_size + oldsize + next_size - newsize;
     if (prev_size + oldsize + next_size >= newsize)
     {
-        char *prev_prev_bp = NULL;
         if (prev_size)
-        {
-            prev_prev_bp = GET_PREV(prev_bp);
             ex_delete(prev_bp);
-        }
-        else
-            prev_prev_bp = GET_PREV(next_bp);
-        
         if (next_size)
             ex_delete(next_bp);
 
+        /* (3*DSIZE may be bigger) */
         if (free_size < 3 * DSIZE)
         {
             if (prev_size)
             {
                 memmove(prev_bp, bp, oldsize - DSIZE);
-                PUT(HDRP(prev_bp), PACK(prev_size + oldsize + next_size, 1));
-                PUT(FTRP(prev_bp), PACK(prev_size + oldsize + next_size, 1));
+                bp = prev_bp;
+            }
 
-                return prev_bp;
-            }
-            else
-            {
-                PUT(HDRP(bp), PACK(prev_size + oldsize + next_size, 1));
-                PUT(FTRP(bp), PACK(prev_size + oldsize + next_size, 1));
-                return bp;
-            }
+            PUT(HDRP(bp), PACK(prev_size + oldsize + next_size, 1));
+            PUT(FTRP(bp), PACK(prev_size + oldsize + next_size, 1));
+            return bp;
         }
 
+        /* split out a free block */
         if (prev_size)
-        {
             memmove(prev_bp, bp, oldsize - DSIZE);
-            PUT(HDRP(prev_bp), PACK(newsize, 1));
-            PUT(FTRP(prev_bp), PACK(newsize, 1));
-            bp = NEXT_BLKP(prev_bp);
-            PUT(HDRP(bp), PACK(free_size, 0));
-            PUT(FTRP(bp), PACK(free_size, 0));
-
-            if ((void *)heap_freep == NULL || (void *)bp < (void *)heap_freep)
-                ex_insert_after(NULL, bp);
-            else
-                ex_insert_after(prev_prev_bp, bp);
-
-            return prev_bp;
-        }
         else
-        {
-            PUT(HDRP(bp), PACK(newsize, 1));
-            PUT(FTRP(bp), PACK(newsize, 1));
             prev_bp = bp;
-            bp = NEXT_BLKP(bp);
-            PUT(HDRP(bp), PACK(free_size, 0));
-            PUT(FTRP(bp), PACK(free_size, 0));
+        PUT(HDRP(prev_bp), PACK(newsize, 1));
+        PUT(FTRP(prev_bp), PACK(newsize, 1));
+        bp = NEXT_BLKP(prev_bp);
+        PUT(HDRP(bp), PACK(free_size, 0));
+        PUT(FTRP(bp), PACK(free_size, 0));
 
-            if ((void *)heap_freep == NULL || (void *)bp < (void *)heap_freep)
-                ex_insert_after(NULL, bp);
-            else
-                ex_insert_after(prev_prev_bp, bp);
+        ex_insert(bp);
 
-            return prev_bp;
-        }
+        return prev_bp;
     }
 
     /* must call malloc for new memory (newsize > oldsize) */
@@ -385,10 +358,14 @@ int mm_check(void)
         return 0;
     }
 
-    /* check explicit list*/
-    if (heap_freep)
+    /* check segregated list*/
+    int class;
+    for (class = 0; class < FCNT; class ++)
     {
-        bp = heap_freep;
+        if (heap_freep[class] == NULL)
+            continue;
+
+        bp = heap_freep[class];
         while (1)
         {
             if (GET_ALLOC(HDRP(bp)))
@@ -398,14 +375,15 @@ int mm_check(void)
             }
             free_cnt--;
             bp = GET_NEXT(bp);
-            if ((void *)bp == (void *)heap_freep)
+            if ((void *)bp == (void *)heap_freep[class])
                 break;
         }
-        if (free_cnt)
-        {
-            fprintf(stderr, "Heap check error: free block count(delta=%d) not consistent!\n", free_cnt);
-            return 0;
-        }
+    }
+
+    if (free_cnt)
+    {
+        fprintf(stderr, "Heap check error: free block count(delta=%d) not consistent!\n", free_cnt);
+        return 0;
     }
 
     return 1;
@@ -416,93 +394,120 @@ int mm_check(void)
  */
 static inline void ex_delete(void *bp)
 {
+    int class;
+
     if (bp == NULL)
         return;
+
+    class = ex_classify(GET_SIZE(HDRP(bp)));
 
     /* bp is the only one in explicit list */
     if (bp == (void *)GET_NEXT(bp))
     {
-        heap_freep = NULL;
+        heap_freep[class] = NULL;
         return;
     }
 
     /* at least two free blocks in explicit list */
-    if (bp == (void *)heap_freep)
-        heap_freep = GET_NEXT(bp);
+    if (bp == (void *)heap_freep[class])
+        heap_freep[class] = GET_NEXT(bp);
     PUT_NEXT(GET_PREV(bp), GET_NEXT(bp));
     PUT_PREV(GET_NEXT(bp), GET_PREV(bp));
 }
 
 /*
  * ex_insert - insert a free block according to the order of address
+ *           - with coalescing
  */
-static void ex_insert(void *bp)
+static void *ex_insert(void *bp)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+    int class = ex_classify(size);
+
+    /* check if there is nearby free block (coalescing) */
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    if (prev_alloc && next_alloc)
+        ;
+    else if (prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        ex_delete(NEXT_BLKP(bp));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+        class = ex_classify(size);
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+        bp = PREV_BLKP(bp);
+        size += GET_SIZE(FTRP(bp));
+        if (!ex_update(bp, size))
+            return bp;
+        class = ex_classify(size);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        ex_delete(NEXT_BLKP(bp));
+        bp = PREV_BLKP(bp);
+        size += GET_SIZE(FTRP(bp));
+        if (!ex_update(bp, size))
+            return bp;
+        class = ex_classify(size);
+    }
+
     /* check if the explicit list is valid */
-    if (heap_freep == NULL)
+    if (heap_freep[class] == NULL)
     {
         PUT_PREV(bp, bp);
         PUT_NEXT(bp, bp);
-        heap_freep = bp;
-        return;
-    }
-
-    /* check if there is nearby free block */
-    if (!GET_ALLOC(FTRP(PREV_BLKP(bp))))
-    {
-        ex_insert_after(PREV_BLKP(bp), bp);
-        return;
-    }
-    if (!GET_ALLOC(HDRP(NEXT_BLKP(bp))))
-    {
-        if ((void *)NEXT_BLKP(bp) == (void *)heap_freep)
-            ex_insert_after(NULL, bp);
-        else
-            ex_insert_after(GET_PREV(NEXT_BLKP(bp)), bp);
-        return;
+        heap_freep[class] = bp;
+        return bp;
     }
 
     /* check if bp < heap_freep */
-    if (bp < (void *)heap_freep)
+    if (bp < (void *)heap_freep[class])
     {
-        ex_insert_after(NULL, bp);
-        return;
+        ex_insert_after(NULL, bp, class);
+        return bp;
     }
 
     /* iterate through the explicit list to find previous free block */
-    char *prev_bp = heap_freep;
+    char *prev_bp = heap_freep[class];
     while (1)
     {
-        if ((void *)GET_NEXT(prev_bp) == (void *)heap_freep)
+        if ((void *)GET_NEXT(prev_bp) == (void *)heap_freep[class])
             break;
         if ((void *)GET_NEXT(prev_bp) > bp)
             break;
         prev_bp = GET_NEXT(prev_bp);
     }
-    ex_insert_after(prev_bp, bp);
+    ex_insert_after(prev_bp, bp, class);
+    return bp;
 }
 
 /*
  * ex_insert_after - insert a free block after an existing free block
+ *                 - without coalescing
  */
-static void ex_insert_after(void *prev_bp, void *bp)
+static void ex_insert_after(void *prev_bp, void *bp, int class)
 {
     /* if prev_bp is NULL then put bp in head */
     if (prev_bp == NULL)
     {
-        if (heap_freep == NULL)
+        if (heap_freep[class] == NULL)
         {
             PUT_PREV(bp, bp);
             PUT_NEXT(bp, bp);
         }
         else
         {
-            PUT_PREV(bp, GET_PREV(heap_freep));
-            PUT_NEXT(bp, heap_freep);
-            PUT_PREV(heap_freep, bp);
+            PUT_PREV(bp, GET_PREV(heap_freep[class]));
+            PUT_NEXT(bp, heap_freep[class]);
+            PUT_PREV(heap_freep[class], bp);
             PUT_NEXT(GET_PREV(bp), bp);
         }
-        heap_freep = bp;
+        heap_freep[class] = bp;
     }
     else
     {
@@ -514,70 +519,30 @@ static void ex_insert_after(void *prev_bp, void *bp)
 }
 
 /*
- * ex_coalesce - coalesce free blocks, return current or new bp
- */
-static void *ex_coalesce(void *bp)
-{
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HDRP(bp));
-
-    /* delete block from free list and update header/footer */
-    if (prev_alloc && next_alloc)
-        return bp;
-    else if (prev_alloc && !next_alloc)
-    {
-        size += GET_SIZE(HDRP(GET_NEXT(bp)));
-
-        ex_delete(GET_NEXT(bp));
-
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    }
-    else if (!prev_alloc && next_alloc)
-    {
-        bp = GET_PREV(bp);
-        size += GET_SIZE(FTRP(bp));
-
-        ex_delete(GET_NEXT(bp));
-
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    }
-    else
-    {
-        size += GET_SIZE(HDRP(GET_NEXT(bp)));
-        ex_delete(GET_NEXT(bp));
-        bp = GET_PREV(bp);
-        size += GET_SIZE(FTRP(bp));
-        ex_delete(GET_NEXT(bp));
-
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    }
-
-    return bp;
-}
-
-/*
  * ex_find_fit - iterate the explicit list to see if there is any free block big enough
  */
 static inline void *ex_find_fit(size_t asize)
 {
-    char *bp = heap_freep;
-    if (bp == NULL)
-        return NULL;
+    int class;
+    char *bp;
 
-    while (1)
+    for (class = ex_classify(asize); class < FCNT; class ++)
     {
-        if (GET_SIZE(HDRP(bp)) >= asize)
-            break;
-        bp = GET_NEXT(bp);
-        if (bp == heap_freep)
-            return NULL;
+        bp = heap_freep[class];
+        if (bp == NULL)
+            continue;
+
+        while (1)
+        {
+            if (GET_SIZE(HDRP(bp)) >= asize)
+                return bp;
+            bp = GET_NEXT(bp);
+            if (bp == heap_freep[class])
+                break;
+        }
     }
 
-    return bp;
+    return NULL;
 }
 
 /*
@@ -592,24 +557,30 @@ static void ex_place(void *bp, size_t asize)
     if ((fsize - asize) >= 3 * DSIZE)
     {
         char *next_bp;
+        int class = ex_classify(fsize - asize);
 
-        PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FTRP(bp), PACK(asize, 1));
-        fsize -= asize;
-        next_bp = NEXT_BLKP(bp);
-        PUT(HDRP(next_bp), PACK(fsize, 0));
-        PUT(FTRP(next_bp), PACK(fsize, 0));
-
-        if ((void *)GET_NEXT(bp) == bp)
+        if (ex_classify(fsize) == class)
         {
-            PUT_PREV(next_bp, next_bp);
-            PUT_NEXT(next_bp, next_bp);
-            heap_freep = next_bp;
+            next_bp = (char *)bp + asize;
+            ex_insert_after(bp, next_bp, class);
+            ex_delete(bp);
+
+            PUT(HDRP(bp), PACK(asize, 1));
+            PUT(FTRP(bp), PACK(asize, 1));
+            PUT(HDRP(next_bp), PACK(fsize - asize, 0));
+            PUT(FTRP(next_bp), PACK(fsize - asize, 0));
         }
         else
         {
-            ex_insert_after(bp, next_bp);
             ex_delete(bp);
+
+            PUT(HDRP(bp), PACK(asize, 1));
+            PUT(FTRP(bp), PACK(asize, 1));
+            next_bp = NEXT_BLKP(bp);
+            PUT(HDRP(next_bp), PACK(fsize - asize, 0));
+            PUT(FTRP(next_bp), PACK(fsize - asize, 0));
+
+            ex_insert(next_bp);
         }
     }
     else
@@ -618,5 +589,56 @@ static void ex_place(void *bp, size_t asize)
 
         PUT(HDRP(bp), PACK(fsize, 1));
         PUT(FTRP(bp), PACK(fsize, 1));
+    }
+}
+
+/*
+ * ex_insert_back - insert a free block(given its size) into the back
+ *                - with coalescing
+ */
+static inline void *ex_insert_back(void *bp, int class)
+{
+    if (!GET_ALLOC(FTRP(PREV_BLKP(bp))))
+        return ex_insert(bp);
+    else if (heap_freep[class] == NULL)
+        ex_insert_after(NULL, bp, class);
+    else
+        ex_insert_after(GET_PREV(heap_freep[class]), bp, class);
+
+    return bp;
+}
+
+/*
+ * ex_classify - determine the appropriate list according to the size
+ */
+static inline int ex_classify(size_t size)
+{
+    int i;
+    for (i = 0; i < FCNT - 1; i++)
+    {
+        if (size <= FS(i))
+            return i;
+    }
+    return FCNT - 1;
+}
+
+/*
+ * ex_update - update a free block's size
+ *           - return: 1 - need to reinsert, 0 - no need
+ */
+static inline int ex_update(void *bp, size_t newsize)
+{
+    if (ex_classify(GET_SIZE(HDRP(bp))) != ex_classify(newsize))
+    {
+        ex_delete(bp);
+        PUT(HDRP(bp), PACK(newsize, 0));
+        PUT(FTRP(bp), PACK(newsize, 0));
+        return 1;
+    }
+    else
+    {
+        PUT(HDRP(bp), PACK(newsize, 0));
+        PUT(FTRP(bp), PACK(newsize, 0));
+        return 0;
     }
 }
